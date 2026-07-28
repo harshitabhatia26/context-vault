3dcd41271660aae7969baed21f43a653696525d5
Harshita Bhatia <bharshita2604@gmail.com>
2026-07-29 01:40:55 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/contextvault/app/ContextVaultApplication.java
@@ -0,0 +1,13 @@
+package com.contextvault.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contextvault/app/ContextVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contextvault.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
